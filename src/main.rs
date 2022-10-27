5ccf38888293b1bfa13fa9cf2b3ace474ac6dc9f
Jan <enderjanyt@gmail.com>
2022-10-27 17:36:19 +0200

    interpret pattern as regex

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 //Import the crates that I used, clap for CLI and colored for colored output
 use clap::Parser;
 use colored::Colorize;
+use regex::Regex;
 use std::fmt;
 use std::fs::File;
 use std::io::prelude::*;
@@ -9,79 +10,87 @@ use std::io::BufReader;
 //Using clap to have cli input
 #[derive(Parser)]
 struct Cli {
-  pattern: String,
-  path: std::path::PathBuf,
+    pattern: String,
+    path: std::path::PathBuf,
 }
 
 //A helper struct for outputting lines
 struct Out {
-  linecount: i32,
-  from: usize,
-  to: usize,
-  text: String,
+    linecount: i32,
+    from: usize,
+    to: usize,
+    text: String,
 }
 
 //Implementation of Display for the Out struct, so I can easily print it with println!("{}", Out)
 impl fmt::Display for Out {
-  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-    // format the highlighted text
-    let higlighted_text = format!(
-      "{}{}{}",
-      // the part before the match
-      &(self.text)[0..self.from],
-      // the part that matches, in blue and bold
-      format!("{}", &(self.text)[self.from..self.to + 1])
-        .bold()
-        .blue(),
-      // the remainder of the line
-      &(self.text)[self.to + 1..]
-    );
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // format the highlighted text
+        let higlighted_text = format!(
+            "{}{}{}",
+            // the part before the match
+            &(self.text)[0..self.from],
+            // the part that matches, in blue and bold
+            format!("{}", &(self.text)[self.from..self.to + 1])
+                .bold()
+                .blue(),
+            // the remainder of the line
+            &(self.text)[self.to + 1..]
+        );
 
-    // actually write the output.
-    write!(
-      f,
-      "l{};c{}: {}",
-      format!("{}", self.linecount).green(),
-      format!("{}", self.from).green(),
-      higlighted_text
-    )
-  }
+        // actually write the output.
+        write!(
+            f,
+            "l{};c{}: {}",
+            format!("{}", self.linecount).green(),
+            format!("{}", self.from).green(),
+            higlighted_text
+        )
+    }
 }
 
 fn main() {
-  //parses the command line args
-  let args = Cli::parse();
+    //parses the command line args
+    let args = Cli::parse();
 
-  //opens the file specified in the args and creates a buffered reader for less memory intensive reading of lines
-  let f = File::open(&args.path).unwrap();
-  let reader = BufReader::new(f);
+    let re: Regex = Regex::new(&args.pattern).unwrap();
+    // let re: Regex = Regex::new(r"version").unwrap();
 
-  //a counter for the line counter output
-  let mut count = 0;
+    //opens the file specified in the args and creates a buffered reader for less memory intensive reading of lines
+    let f = File::open(&args.path).unwrap();
+    let reader = BufReader::new(f);
 
-  //loops through all lines in a file
-  for line in reader.lines() {
-    count = count + 1;
-    match line {
-      // if we were able to read the line
-      Ok(text) => {
-        // If we can find an index that matches the pattern
-        if let Some(index) = text.find(&args.pattern) {
-          // Create the helper struct, for which we implemented the Display trait
-          let line_out = Out {
-            linecount: count,
-            from: index,
-            to: index + &args.pattern.len() - 1,
-            text,
-          };
-          // print the result
-          println!("{}", line_out);
+    //a counter for the line counter output
+    let mut count = 0;
+
+    //loops through all lines in a file
+    for line in reader.lines() {
+        count = count + 1;
+        match line {
+            // if we were able to read the line
+            Ok(text) => {
+                // the text as str, for the regex
+                let text_as_str = text.as_str();
+
+                // get the matches in this str
+                let matches = re.find_iter(&text_as_str);
+
+                // for each found match we output it
+                for found_match in matches {
+                    let line_out = Out {
+                        linecount: count,
+                        from: found_match.start(),
+                        to: found_match.end()-1,
+                        text: text_as_str.to_string(),
+                    };
+                    println!("{}", line_out);
+                }
+
+            }
+            // Handle any errors.
+            Err(error) => {
+                println!("{}", error)
+            }
         }
-      }
-      // Handle any errors.
-      Err(error) => {
-        println!("{}", error)
-      }
     }
-  }
 }
