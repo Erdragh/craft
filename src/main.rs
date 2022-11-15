e4482008a7fb4fc6425a705d5d1c896ccf7380cb
Jan <enderjanyt@gmail.com>
2022-11-15 14:19:16 +0100

    read from stdin and handle lines like grep

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use clap::Parser;
 use colored::Colorize;
 use regex::Regex;
 use std::fmt;
+use std::io::stdin;
+use std::io::Result;
 use std::fs::File;
 use std::io::prelude::*;
 use std::io::BufReader;
@@ -13,7 +15,7 @@ struct Cli {
     /// The pattern that will be checked for. This is interpreted as a regex, so be careful with special characters
     pattern: String,
     /// The file that will be checked.
-    path: std::path::PathBuf,
+    path: Option<std::path::PathBuf>,
 }
 
 //A helper struct for outputting lines
@@ -51,48 +53,56 @@ impl fmt::Display for Out {
     }
 }
 
-fn main() {
+fn main() -> Result<()> {
+
     //parses the command line args
     let args = Cli::parse();
 
-    let re: Regex = Regex::new(&args.pattern).unwrap();
+    let regex: Regex = Regex::new(&args.pattern).unwrap();
     // let re: Regex = Regex::new(r"version").unwrap();
 
-    //opens the file specified in the args and creates a buffered reader for less memory intensive reading of lines
-    let f = File::open(&args.path).unwrap();
-    let reader = BufReader::new(f);
-
-    //a counter for the line counter output
-    let mut count = 0;
-
-    //loops through all lines in a file
-    for line in reader.lines() {
-        count = count + 1;
-        match line {
-            // if we were able to read the line
-            Ok(text) => {
-                // the text as str, for the regex
-                let text_as_str = text.as_str();
-
-                // get the matches in this str
-                let matches = re.find_iter(&text_as_str);
-
-                // for each found match we output it
-                for found_match in matches {
-                    let line_out = Out {
-                        linecount: count,
-                        from: found_match.start(),
-                        to: found_match.end()-1,
-                        text: text_as_str.to_string(),
-                    };
-                    println!("{}", line_out);
+    if let Some(path) = &args.path {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        let mut count = 0;
+        for line in reader.lines() {
+            match line {
+                Ok(line_string) => {
+                    match_and_output(&regex, line_string.as_str(), count);
                 }
-
+                Err(_) => {}
             }
-            // Handle any errors.
-            Err(error) => {
-                println!("{}", error)
-            }
+            count += 1;
         }
+    } else {
+        let stdin = stdin();
+        let handle = stdin.lock();
+
+        let mut count = 0;
+        for line in handle.lines() {
+            match line {
+                Ok(line_string) => {
+                    match_and_output(&regex, line_string.as_str(), count);
+                }
+                Err(_) => {}
+            }
+            count += 1;
+        }
+    }
+    Ok(())
+}
+
+fn match_and_output(regex: &Regex, line: &str, line_count: i32) {
+    let matches = regex.find_iter(&line);
+
+    // for each found match we output it
+    for found_match in matches {
+        let line_out = Out {
+            linecount: line_count,
+            from: found_match.start(),
+            to: found_match.end()-1,
+            text: line.to_string(),
+        };
+        println!("{}", line_out);
     }
 }
